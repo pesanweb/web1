b3bb87402cf8cebaa4c851300bc395979d82d643
andyprodistik <145884493+andyprodistik@users.noreply.github.com>
2026-05-10 08:40:20 +0700

    feat: add build:dev script and remove service worker injection from development configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const path = require('path');
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -20,10 +19,6 @@ module.exports = merge(common, {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new InjectManifest({
-      swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
-      swDest: 'sw.js',
-    }),
   ],
   devServer: {
     static: path.resolve(__dirname, 'dist'),
